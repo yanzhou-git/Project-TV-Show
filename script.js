7940826dc4f2ad0c50fe2a70e08cc81b034e0a04
Yan <nubelaprofesora2021@gmail.com>
2026-02-01 20:07:23 +0100

    level100

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(allEpisodes) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+
+  allEpisodes.forEach(({ name, season, number, image, summary }) => {
+    const seasonEpisode = `S${String(season).padStart(2, "0")}E${String(number).padStart(2, "0")}`;
+    const clone = document
+      .getElementById("film-template")
+      .content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = `${name} - ${seasonEpisode}`;
+    clone.querySelector("img").src = image.medium;
+    clone.querySelector("img").alt = name;
+    clone.querySelector("p").innerHTML = summary;
+
+    rootElem.appendChild(clone);
+  });
 }
 
 window.onload = setup;
